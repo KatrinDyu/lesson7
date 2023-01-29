be2c7b95cfd627eec117de02bed8b946202b5e32
Ekaterina <dyuldina32@gmail.com>
2023-01-29 19:54:57 +0500

    pomenyala na double. ispravila ex1

--- ex1/Program.cs
+++ ex1/Program.cs
@@ -8,28 +8,28 @@ int rows = int.Parse(Console.ReadLine());
 Console.Write("Введите количество столбцов массива n: ");
 int columns = int.Parse(Console.ReadLine());
 
-int[,] array = GetArray(rows, columns, 0, 10);
+double[,] array = GetArray(rows, columns, 0, 10);
 PrintArray(array);
 
-int[,] GetArray(int m, int n, int minValue, int maxValue)
+double[,] GetArray(int m, int n, int minValue, int maxValue)
 {
-    int[,] result = new int[m, n];
+    double[,] result = new double[m, n];
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
 {
-            result[i, j] = new Random().Next(minValue, maxValue + 1);
+            result[i, j] = new Random().NextDouble() * (maxValue - minValue);
         }
     }
     return result;
 }
-void PrintArray(int[,] inArray)
+void PrintArray(double[,] inArray)
 {
     for (int i = 0; i < inArray.GetLength(0); i++)
     {
         for (int j = 0; j < inArray.GetLength(1); j++)
         {
-            Console.Write($"{inArray[i,j]} ");
+            Console.Write($"{inArray[i,j]:f2} ");
         }
         Console.WriteLine();
     }
---
+++ ex2/Program.cs
@@ -0,0 +1,45 @@
+﻿/* Напишите программу, которая на вход принимает позиции 
+элемента в двумерном массиве, и возвращает значение этого 
+элемента или же указание, что такого элемента нет. */
+Console.Clear();
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+{
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+onsole.Write("Введите координату i элемента: ");
+int coordinatai = int.Parse(Console.ReadLine());
+
+Console.Write("Введите координату y элемента: ");
+int coordinatay = int.Parse(Console.ReadLine());
+int SearchElement(int [,] array, int el)
+{
+    if (x = )
+}
